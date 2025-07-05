99aed2354f4a64040ce1390b58bf1a0d61974e78
Ganesh Shireesha <sireeshaganesh04@gmail.com>
2025-07-05 15:15:38 +0530

    update

--- java_fundamentals/src/java_fundamentals/FCS3.java
+++ java_fundamentals/src/java_fundamentals/FCS3.java
@@ -1,23 +1,22 @@
 package java_fundamentals;
+import java.util.Scanner;
 
 public class FCS3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		if(args.length==0) {
-			System.out.println("No value");
-		}
-		else {
-			for(int i=0;i<args.length;i++) {
-				System.out.println(args[i]);
-				if(i<args.length -1) {
-					System.out.print(", ");
-				}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("No values");
+			String s = sc.nextLine();
+			if(s=="") {
+				System.out.println("No Values");
 			}
-			System.out.println();
-		}
-
+			else {
+				String[] words=s.split(" " );
+				String result = String.join("," , words);
+				System.out.println(result);
+			}
+			sc.close();
 	}
 
 }
---
+++ java_fundamentals/src/java_fundamentals/FCS7.java
@@ -0,0 +1,25 @@
+package java_fundamentals;
+
+import java.util.Scanner;
+
+public class FCS7 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		 System.out.print("Enter a single alphabet character: ");
+	        char ch = sc.next().charAt(0);
+
+	        if (ch >= 'a' && ch <= 'z') {
+	            char upper = (char)(ch - 32);
+	            System.out.println(ch + "->" + upper);
+	        } else if (ch >= 'A' && ch <= 'Z') {
+	            char lower = (char)(ch + 32);
+	        
+	            System.out.println(ch + "->" + lower);
+	        }
+	            else {
+	            	System.out.println("Invalid Input");
+	            }
+	        sc.close();
+	        
+	    }
+}
