d83ed3386c0d5c84b9b8d6729e1e3b64fa748cde
Ganesh Shireesha <sireeshaganesh04@gmail.com>
2025-07-17 12:55:39 +0530

    wrapper_classes

---
+++ wrapper_classes/src/wrapper_classes/WrapperClass3.java
@@ -0,0 +1,20 @@
+package wrapper_classes;
+import java.util.Scanner;
+
+public class WrapperClass3 {
+	public static void main(String[]args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		
+		int num=sc.nextInt();
+		if(num < 1 || num > 255) {
+			System.out.println("Invalid output");
+		}
+		else {
+			String binary=Integer.toBinaryString(num);
+			String digit=String.format("%8s", binary).replace(' ', '0');
+			System.out.println("Output Contain 8 digits: "+digit);
+		}
+	}
+}
